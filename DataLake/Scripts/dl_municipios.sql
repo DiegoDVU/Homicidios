99bbf9dbfee62ad7ea0533bedfe63a01e093b852
DiegoDVU <yeyos0803@hotmail.com>
2025-05-23 21:31:28 -0500

    Add files via upload

---
+++ DataLake/Scripts/dl_municipios.sql
@@ -0,0 +1,16 @@
+-- DataLake.dbo.dl_municipios definition
+
+-- Drop table
+
+-- DROP TABLE DataLake.dbo.dl_municipios;
+
+CREATE TABLE DataLake.dbo.dl_municipios (
+	COD_DPTO tinyint NULL,
+	NOM_DPTO nvarchar(60) NULL,
+	COD_MPIO int NULL,
+	NOM_MPIO nvarchar(30) NULL,
+	TIPO nvarchar(25) NULL,
+	LATITUD float NULL,
+	LONGITUD float NULL,
+	[Geo Municipio] varchar(50) NULL
+);
